e825d9895f3faaa67fabc9f3771ae8433999c8b8
CompuMega <CompuMega@DESKTOP-T82KGN9>
2021-08-05 15:46:27 -0500

    Agregar archivos de proyecto.

---
+++ EjemploIterator/Palabra.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploIterator
+{
+    public class Palabra : IEnumerable
+    {
+        String _palabra;
+        public Palabra(string palabra)
+        {
+            _palabra = palabra;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new PalabraIterator(_palabra);
+        }
+    }
+}
---
+++ EjemploIterator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploIterator
+{
+  class Program
+    {
+        static void Main (string[]args)
+
+       {
+           // Palabra palabra = new Palabra("Soy Nuevo");
+            //foreach(var letra in palabra)
+            //{
+             //   Console.WriteLine(letra);
+            //}
+            foreach (var item in "Soy Nuevo")
+            {
+            Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    
+
+    }
+}
